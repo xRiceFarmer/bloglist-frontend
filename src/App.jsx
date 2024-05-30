dc9e902fc6f4109399c2b47a72c7c5313bed0c06
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-05-30 15:32:04 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,159 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import Notification from "./components/Notification";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogUser", JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      const messageObject = {
+        text: "Wrong username or password",
+        type: "error",
+      };
+      setMessage(messageObject);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
+  const handleLogout = (event) => {
+    window.localStorage.removeItem("loggedBlogUser");
+    setUser(null);
+  };
+  const createBlog = async (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url,
+    };
+    try {
+      const response = await blogService.create(blogObject);
+      setBlogs(blogs.concat(response));
+      const messageObject = {
+        text: `a new blog ${title} by ${author} added`,
+        type: "success",
+      };
+      setMessage(messageObject);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+      setAuthor("");
+      setTitle("");
+      setUrl("");
+    } catch (error) {
+      const messageObject = {
+        text: error.message,
+        type: "error",
+      };
+      setMessage(messageObject);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
+  const loginForm = () => (
+    <div>
+      <Notification message={message} />
+
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+  if (user === null) {
+    return loginForm();
+  }
+  return (
+    <div>
+      <Notification message={message} />
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      <button onClick={handleLogout}>logout</button>
+      <h2>create new</h2>
+      <form onSubmit={createBlog}>
+        <div>
+          Title:
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          Author:
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          URL:
+          <input
+            type="text"
+            value={url}
+            name="URL"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/services/blogs.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const baseUrl = "/api/blogs";
+
+let token = null;
+const setToken = newToken => {
+  token = `Bearer ${newToken}`;
+};
+
+const create = async (newObject) => {
+  const config = { headers: { Authorization: token } };
+  const response = await axios.post(baseUrl, newObject, config);
+  return response.data;
+};
+const getAll = async () => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.get(baseUrl, config);
+  return response.data;
+};
+
+export default { getAll, setToken, create };
