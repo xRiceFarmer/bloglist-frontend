897ebdc1b90286caa6295f2218997fc9256e4a24
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-06-18 13:07:53 +0700

    finished 7.12

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
 const Notification = ({ message }) => {
-  if (message === null) {
+  if (!message.text) {
     return null
   }
   return <div className={message.type}>{message.text}</div>
